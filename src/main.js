a33861fad15eb1716654c532506e93b94908ea82
Duban Sierra <univerdidad.duban@gmail.com>
2021-11-19 23:00:25 -0500

    configuracion del proyecto

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootswatch/dist/flatly/bootstrap.min.css";
 
 createApp(App).use(store).use(router).mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
 import { createStore } from "vuex";
+import state from "./states";
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state,
+  getters,
+  mutations,
+  actions,
   modules: {},
 });
